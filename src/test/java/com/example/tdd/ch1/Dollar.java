c9067f5be4dbdd147e829c3ff4d5bbe5e10fea8e
김정근 <gogoogjg@tripstore.kr>
2020-07-11 17:54:47 +0900

    add a failed test

---
+++ src/test/java/com/example/tdd/ch1/Dollar.java
@@ -0,0 +1,13 @@
+package com.example.tdd.ch1;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int i) {
+
+    }
+
+    public void times(int i) {
+
+    }
+}
--- src/test/java/com/example/tdd/ch1/DollarTest.java
+++ src/test/java/com/example/tdd/ch1/DollarTest.java
@@ -7,7 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DollarTest {
 
     @Test
-    void test() {
-        assertEquals(true, true);
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
     }
 }

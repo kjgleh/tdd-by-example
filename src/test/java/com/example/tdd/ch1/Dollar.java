cd564200a5a66c9181e2b080591c3d9967171ca6
김정근 <gogoogjg@tripstore.kr>
2020-07-11 17:57:54 +0900

    fix the failed test

--- src/test/java/com/example/tdd/ch1/Dollar.java
+++ src/test/java/com/example/tdd/ch1/Dollar.java
@@ -1,13 +1,14 @@
 package com.example.tdd.ch1;
 
 public class Dollar {
+    
     public int amount;
 
-    public Dollar(int i) {
-
+    public Dollar(int amount) {
+        this.amount = amount;
     }
 
-    public void times(int i) {
-
+    public void times(int multiplier) {
+        this.amount *= multiplier;
     }
 }

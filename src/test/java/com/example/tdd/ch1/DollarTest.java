a7b20cc96c3d0eb72d1822f6c0ac7537605befa6
김정근 <gogoogjg@tripstore.kr>
2020-07-11 17:51:21 +0900

    initialize project

---
+++ src/test/java/com/example/tdd/ch1/DollarTest.java
@@ -0,0 +1,13 @@
+package com.example.tdd.ch1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+
+    @Test
+    void test() {
+        assertEquals(true, true);
+    }
+}
